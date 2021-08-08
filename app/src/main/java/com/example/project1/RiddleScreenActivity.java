3794666283a434bc0b1165cb89abb3e00dcb464a
jedominguez <78039980+YELO-icy@users.noreply.github.com>
2021-08-08 16:59:15 -0600

    Added and implemented a ui of title screen and methods

---
+++ app/src/main/java/com/example/project1/RiddleScreenActivity.java
@@ -0,0 +1,34 @@
+package com.example.project1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class RiddleScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.riddle_screen);
+    }
+
+    String[] riddles = new String[15];
+
+
+    public void getRiddles(){}
+
+    public void changeRiddles(){}
+
+    public void Timer(){}
+
+    public void Retry(View v){}
+
+    public void displayText(){}
+
+    public void textDisplay(){}
+
+    public void inputBox(View v){}
+
+
+}
